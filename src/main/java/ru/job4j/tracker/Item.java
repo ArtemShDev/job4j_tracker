91cd705decdd43008e4cfa039777522158bde95f
ArtemShDev <dev.art.balt@gmail.com>
2021-06-14 09:21:11 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,7 +3,14 @@ package ru.job4j.tracker;
 public class Item {
     private int id;
     private String name;
-
+    public Item() {}
+    public Item(String name) {
+        this.name = name;
+    }
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
     public int getId() {
         return id;
     }
