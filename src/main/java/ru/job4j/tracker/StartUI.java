5d24ad63f1869a35bd6a2396cf699424012e9142
ArtemShDev <dev.art.balt@gmail.com>
2021-06-15 14:05:09 +0300

    6. Date. Отображение даты. [#310017 #167712]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,7 +6,7 @@ public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
         LocalDateTime dateTime = item.getDateTime();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(dateTime.format(formatter));
     }
 
