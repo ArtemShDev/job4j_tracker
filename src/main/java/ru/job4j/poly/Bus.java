5e7ad5c19dd79b78a473ba5110e7f697533fbb5c
ArtemShDev <dev.art.balt@gmail.com>
2021-06-26 14:51:49 +0300

    3. Интерфейс Input [#181092 #171601]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -5,11 +5,13 @@ public class Bus implements Transport{
     @Override
     public void drive() {
         boolean canDrive = false;
+        System.out.println("Calling the method \"drive\"");
     }
 
     @Override
     public void passengers(int passengers) {
         int chairs = passengers;
+        System.out.println("Calling the method \"passengers\"");
     }
 
     @Override
