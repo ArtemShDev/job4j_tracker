4595e644c68aca4cffa449ad2bd876655cb63b23
ArtemShDev <dev.art.balt@gmail.com>
2021-08-03 14:37:49 +0300

    2.2. Лямбда блок [#249208 #185213]

--- src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -1,6 +1,8 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class LambdaUsage {
     public static void main(String[] args) {
@@ -8,5 +10,12 @@ public class LambdaUsage {
             System.out.println("Compare " + right.length() + " : " + left.length());
             return Integer.compare(right.length(), left.length());
         };
+        List<String> list = Arrays.asList(
+                "FI",
+                "Func Inter",
+                "Lambda"
+        );
+        list.sort(cmpDescSize);
+        System.out.println(list);
     }
 }
