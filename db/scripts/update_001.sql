3a5e4a74e2fd71c95b3db92ce9e3bb39594af04a
ArtemShDev <dev.art.balt@gmail.com>
2021-12-16 18:20:48 +0300

    0. Что такое Mock-объект. [#6870]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -2,4 +2,5 @@ create table if not exists items (
     id serial primary key,
     name text,
     created timestamp
+
 );
---
+++ src/main/java/ru/job4j/tracker/MemTrackerT.java
@@ -0,0 +1,52 @@
+package ru.job4j.tracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemTrackerT {
+
+    private final Output out;
+
+    public MemTrackerT(Output out) {
+        this.out = out;
+    }
+
+    public void init(Input input, Store memTracker, List<UserAction> actions) {
+        boolean run = true;
+        while (run) {
+            showMenu(actions);
+            int select = input.askInt("Select:");
+            if (select < 0 || select >= actions.size()) {
+                out.println("Wrong input, you can select: 0 .. " + (actions.size() - 1));
+                continue;
+            }
+            UserAction action = actions.get(select);
+            run = action.execute(input, memTracker);
+        }
+    }
+
+    private void showMenu(List<UserAction> actions) {
+        out.println("Menu:");
+        for (int i = 0; i < actions.size(); i++) {
+            out.println(i + ". " + actions.get(i).name());
+        }
+    }
+
+    public static void main(String[] args) {
+        Output output = new ConsoleOutput();
+        Input input = new ValidateInput(output, new ConsoleInput());
+        MemTracker memTracker = new MemTracker();
+        //sqlTracker.init();
+        List<UserAction> actions = new ArrayList<>();
+        actions.add(new CreateAction(output));
+        actions.add(new ShowAllAction(output));
+        actions.add(new EditAction(output));
+        actions.add(new DeleteAction(output));
+        actions.add(new FindByIdAction(output));
+        actions.add(new FindByNameAction(output));
+        actions.add(new ExitAction());
+        new StartUI(output).init(input, memTracker, actions);
+    }
+
+}
+
